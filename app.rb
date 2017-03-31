b7bcec8af09343903c366dc845c4f0cd00d81078
Beata Obrok <beata.obrok@u2i.com>
2017-03-31 18:20:16 +0200

    Stuff

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ post '/' do
     client_id: ENV['CLIENT_ID'],
     client_secret: ENV['CLIENT_SECRET']
   )
+  id = ''
+  prs = client.pull_requests('quirk0o/pivotal-github-integration')
+  pr = prs.find { |pr| pr[:title].match(id) }
+  commits = client.pull_request_commits('quirk0o/pivotal-github-integration', pr.number)
 
   payload = JSON.parse(request.body.read)
   puts payload
