a2db6aa0612ec3d55821e75cc2df92ae76c320e6
Beata Obrok <beata.obrok@u2i.com>
2017-04-27 18:20:29 +0200

    Tests

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ post '/github/:pivotal_project_id' do
     access_token: ENV['ACCESS_TOKEN']
   )
 
+  puts headers['X-GitHub-Event']
+
   payload = JSON.parse(request.body.read)
   puts payload
   repo = payload['repository']['full_name']
